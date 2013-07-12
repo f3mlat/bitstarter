e1556c18f1ba7089b41848f1694d09bf421613ad
Lateef <f3mlat@yahoo.com>
2013-07-12 02:40:31 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+var infile = "index.html";
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = fs.readFileSync(infile, "utf8");
+  var buffer = new Buffer(content, "utf8");
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
